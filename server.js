be842c76c4364c7eb7606ce0ed14cc5a5e1545a7
Cody Chase <cchase1991@gmail.com>
2017-08-12 10:14:58 -0400

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var methodOverride= require("method-override");
 var bodyParser= require("body-parser");
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var exphbs = require("express-handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
